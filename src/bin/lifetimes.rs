2a48bbdf788c92dfcd59d3f7a44d0f5debdccc49
f3kilo <f3kilo@yandex.ru>
2023-02-09 19:47:03 +0300

    lifetime elision

--- src/bin/lifetimes.rs
+++ src/bin/lifetimes.rs
@@ -14,3 +14,36 @@ fn foo<'a, T: 'a>(_t: T) {}
 fn bar<T>(_t: &T) {}
 
 struct RefWrapper<'a, T>(&'a T);
+
+fn _elided(_s: &str) {}
+
+fn _elided2(_s: &str) -> &str {
+    todo!()
+}
+
+fn _elided3(_s1: &str) -> RefWrapper<i32> {
+    todo!()
+}
+
+// fn not_elided2(_s1: &str, _s2: &str) -> &str {
+//     todo!()
+// }
+
+struct _A;
+
+impl _A {
+    fn _elided(&self, _s2: &str) -> &str {
+        todo!()
+    }
+
+    fn _elided2(&self, _s2: &str) -> RefWrapper<i32> {
+        todo!()
+    }
+}
+
+// struct ClosureWrapper<F>
+// where
+//     F: Fn(&str, &str) -> &str,
+// {
+//     closure: F,
+// }
